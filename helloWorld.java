dc84eae05167f34c086b710a577f721470e5ca4c
miikachu <miikachu22@gmail.com>
2018-09-18 16:02:27 -0400

    added java hello world program

---
+++ helloWorld.java
@@ -0,0 +1,10 @@
+//9-18-18 First Content on GitHub
+/*
+*function prints Hello World! to user
+*/
+
+#include namespace std;
+
+public static void main(String[] args) {
+  system.out.println("Hello World!");
+}
